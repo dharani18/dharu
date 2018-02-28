bb24d0dd7bdb31848d6be78eda56e00e28626ccc
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-02-28 17:50:53 +0530

    Create even.cpp

---
+++ even.cpp
@@ -0,0 +1,15 @@
+using namespacestd;
+int main()
+{
+ int num,m =10,n=20;
+printf("Print even Numbers" );
+ for (num = m; num <= n; num++)
+
+        {
+
+               if (num % 2 == 0)
+
+                  printf ("%d ", num);
+
+         }
+{
