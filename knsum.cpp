fb865917e769763e8e19ab217ed6e5b2097c93f6
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-02-28 17:18:47 +0530

    Create knsum.cpp

---
+++ knsum.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+  int i,k,N,sum=0,a[N];
+  cout<<"enter value of k,N";
+  cin>>k>>N;
+  for(i=0;i<=n;i++)
+  {
+      sum=sum+i;
+      cout<<"sum is="<<sum<<endl;
+      cin>>sum;
+  }
+  }
