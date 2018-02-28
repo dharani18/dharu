7b89a9ca32b5ef45d37ee9e107b29c9a7c1adbec
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-02-28 17:48:57 +0530

    Create odd.cpp

---
+++ odd.cpp
@@ -0,0 +1,16 @@
+using namespacestd;
+int main()
+{
+ int num,m =10,n=20;
+printf("Print Odd Numbers" );
+ for (num = m; num <= n; num++)
+
+        {
+
+               if (num % 2 == 1)
+
+                  printf ("%d ", num);
+
+         }
+{
+	
