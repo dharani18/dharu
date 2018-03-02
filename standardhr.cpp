64b13c03141245fcf2e3f163a68f280c0203790d
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-02 21:12:33 +0530

    Create standardhr.cpp

---
+++ standardhr.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespacestd;
+
+int hourmin(void)
+int main()
+
+{
+int hour;
+cout<<"Enter number of hours:"<< hour << endl;
+cin>>hour;
+int min;
+cout<<"Enter number of minuets:" << min << endl;
+cin>>min;
+int hourmin;
+cout<<"Time:"<< hourmin;
+
+cin.get();
+cin.get();
+
+return 0;
+}
+
+int hourmin(void)
+{
+int hour, int min;
+}
