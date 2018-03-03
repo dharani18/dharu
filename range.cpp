47187af3f5c282f9c768787f77d96346bdb62d1f
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-03 12:03:29 +0530

    Create range.cpp

---
+++ range.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespacestd;
+int x,n;
+cout<<enter n;
+cin>>n;
+bool inrange(1,10,x)
+{
+return(1<=n && n<=10);
+}
+int main()
+{
+inRange(1,10,50)?
+cout<<"yes\n";
+else
+cout<<"no\n";
+return 0;
+}
