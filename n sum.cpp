61f7ec44b8c1aab3fa8644dc4bda9ff721567a96
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-20 20:44:09 +0530

    Create n sum.cpp

---
+++ n sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i, n, sum=0, num;
+	cout<<"How many number?";
+	cin>>n;
+	cout<<"Enter "<<n<<" number :";
+	for(i=0;i<n;i++)
+	{
+		cin>>num;
+		sum=sum+num;
+	}
+	cout<<"Sum of all the "<<n<<" number is "<<sum;
+	return 0;
+}
