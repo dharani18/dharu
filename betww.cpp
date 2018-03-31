e601b9c3700f0bc75d9e0f36f770d7eefd333111
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-31 16:59:28 +0530

    Create betww.cpp

---
+++ betww.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int n;
+   cout<<"enter the number";
+   cin>>n;
+   int n2,n3;
+   cout<<"enter the first num";
+   
+   cin>>n2;
+   cout<<"enter the second num";
+   cin>>n3;
+   if(n>=n2&&n<=n3)
+   {
+       cout<<"yes";
+   }
+   else
+   {
+       cout<<"no";
+   }
+    return 0;
+}
