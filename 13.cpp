2f56391ffa17b4d5566abb9b09c0e40fbc61bf63
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-03-31 17:36:02 +0530

    Create 13.cpp

---
+++ 13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int n;
+   int res;
+   cout<<"enter the number :";
+   cin>>n;
+   if(n%13==0)
+   {
+       cout<<"yes,multiple of 13";
+   }
+   else
+   {
+       cout<<"no,not multiple of 13";
+   }
+}
