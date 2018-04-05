f9761b86826f8be45006736a9ad7ec617c4725e3
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-04-05 19:42:19 +0530

    Create mmm.cpp

---
+++ mmm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n,n1,n2,res,res1;
+    cout<<"enter the numbers :";
+    cin>>n>>n1>>n2;
+    res=n*n1;
+    res1=res%n2;
+    cout<<"answer for number1*number2%number3:"<<res1;
+    return 0;
+}
